9abd0c39f4ada8ac7510d91cf4e8f743776dc1be
matiE98 <71802425+matiE98@users.noreply.github.com>
2022-09-26 14:56:50 -0300

    Añadido control de tiempo de riego

--- CodigoLDR.ino
+++ CodigoLDR.ino
@@ -7,7 +7,10 @@ int potencia = 0;
 int luz = 0;
 int zona = 1; // Zona donde se está regando actualmente, tomará valores del 1 al 3
 int apagado = 0; // Bandera para controlar el estado apagado/encendido
-int unaVuelta = 2048;
+int giro = 512;
+int vueltas = 1;
+unsigned long tiempo = 0;
+long tiempoUltimoCambio = 0;
 
 void setup() 
 {
@@ -16,78 +19,68 @@ void setup()
 
 void loop()
 {
-  regar(4096); // 2 vueltas en la zona 1 (aprox 40 segundos)  
-  zona = zona + 1;
-  delay(10000);  
-
-  regar(6144); // 3 vueltas en la zona 2 (aprox 60 segundos)
-  zona = zona + 1;
-  delay(10000);
+  tiempo = millis();
+  tiempoUltimoCambio = tiempo;
+  regar(20000); // Riega zona 1 durante 20segs.
+  zona++;
+  vueltas=1;  
+  delay(5000);  
   
-  regar(8192); // 4 vueltas en la zona 3 (aprox 80 segundos)
+  tiempoUltimoCambio = tiempo;
+  regar(25000); // Riega zona 2 durante 25segs.
+  zona++;
+  vueltas=1;
+  delay(5000);
+
+  tiempoUltimoCambio = tiempo;
+  regar(30000); // Riega zona 3 durante 30segs.
   
   apagarMotor();
   delay(20000); // Una vez completado el ciclo de zonas (cerca del mediodia), el motor volverá a regar 20 segs después
   zona = 1;
-
-  regar(2048); // 1 vuelta en la zona 1 (aprox 20 segundos)
-  zona = zona + 1;
-  delay(10000);  
+  vueltas=1;
   
-  regar(4096); // 2 vueltas en la zona 2 (aprox 40 segundos)
-  zona = zona + 1;
-  delay(10000);
+  tiempoUltimoCambio = tiempo;
+  regar(20000); // Riega zona 1 durante 20segs.
+  zona++;
+  vueltas=1;
+  delay(5000);  
   
-  regar(6144); // 3 vueltas en la zona 3 (aprox 60 segundos)
+  tiempoUltimoCambio = tiempo;
+  regar(25000); // Riega zona 2 durante 25segs.
+  zona++;
+  vueltas=1;
+  delay(5000);
+  
+  tiempoUltimoCambio = tiempo;
+  regar(30000); // Riega zona 3 durante 30segs.
   apagarMotor();
   zona = 1; 
+  vueltas=1;
+  delay(20000);  
 }
 
 void obtenerDatos()
 {
   luz = analogRead(PIN_LUZ);
-	mapear(luz); 
+	velocidad = map(luz, 0, 1023, 1, 4);
 }
 
-void mapear(int l)
+void regar(int duracion) 
 {
-  if (l <= 250) {
-    velocidad = 1;
-  }
-  else if (l > 250 && l <= 500) {
-    velocidad = 2;
-  }
-  else if (l > 500 && l <= 750)  {
-    velocidad = 3;
-  }
-  else if (l > 750 && l <= 1023){
-    velocidad = 4; // Significa que es de noche
-  };  
-}
-
-void regar(int pasos) 
-{
-  for (int i = 0; i < (pasos/unaVuelta); i++ ) // Entre cada vuelta el motor va a tomar una nueva medida de la intensidad de la luz
-  {
-    while ( velocidad == 4) // Mientras sea de noche el motor permanecerá apagado
-    { 
-      if (apagado = 0) { // Si el motor está encendido se apagará
-        apagarMotor();
-      }
-	    obtenerDatos(); // El microcontrolador seguirá tomando datos cada 1 segundo hasta que sea de día, entonces comenzará a regar    
-      delay(1000);
-    } 
-    controlarMotor(pasos);
+  while ( tiempo - tiempoUltimoCambio < duracion) // Controla que el motor gire durante el tiempo pasado por parametro 
+  {                                                 // (al ser una duracion pequeña para la demo, puede pasarse unos 7 segs.)
     obtenerDatos();
-  }    
-  delay(1000);
+    controlarMotor(); 
+    tiempo=millis();
+    delay(1000);
+  };  
 }
 
 // La velocidad va a ser 4 cuando la fotoresistencia no detecte luz, es decir por la noche. 
 // Va a ser 1 cuando la intensidad de la luz sea maxima, es decir en horario cercano al mediodia en condiciones ideales. 
 // El motor debe permanecer apagado cuando la velocidad sea 4. 
-// A una velocidad de 3 rpm hace 3 vueltas en 1 minuto --> en 60 minutos (1 hora) dará 180 vueltas.
-void controlarMotor(int pasos) 
+void controlarMotor() 
 {    
   if (velocidad < 4) { 
     motor1.setSpeed(velocidad); // El motor girará a la velocidad obtenida de mapear los datos recibidos por la fotoresistencia 
@@ -97,15 +90,23 @@ void controlarMotor(int pasos)
     Serial.print(velocidad);
     Serial.println(" rpms");
     Serial.print("cantidad de vueltas: ");
-    Serial.println(pasos/unaVuelta);
-    Serial.print("intensidad de luz: ");    
+    Serial.println(vueltas);
+    Serial.print("lectura de luz: ");    
     Serial.println(luz);
     Serial.println("");
-    motor1.step(unaVuelta); // El motor realizará la cantidad de pasos equivalentes a una vuelta 
-    apagado = 0;     
+    motor1.step(giro); // El motor realizará la cantidad de pasos pasada por parametros (512, 1/4 de vuelta)
+    apagado = 0;
+    vueltas++;     
   }
 	else if ( velocidad == 4 ) {
-    apagarMotor(); 
+    if (apagado = 0) { // Si el motor está encendido se apagará
+      apagarMotor();
+    }; 
+    tiempo = tiempo - 1000;
+    Serial.println("Motor apagado");  
+    Serial.print("lectura de luz: ");    
+    Serial.println(luz); 
+    Serial.println("");
   } 
 }
 
